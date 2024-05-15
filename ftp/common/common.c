1ddddc4f1b30d8cae75ccad649cee2c56864b241
Максентий <113115807+Maksentiu@users.noreply.github.com>
2024-05-15 19:49:04 +0300

    Update common.c

--- ftp/common/common.c
+++ ftp/common/common.c
@@ -1,23 +1,16 @@
 #include "common.h"
 
-
-/**
- * Create listening socket on remote host
- * Returns -1 on error, socket fd on success
- */
 int socket_create(int port)
 {
 	int sockfd;
 	int yes = 1;
 	struct sockaddr_in sock_addr;
 
-	// create new socket
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("socket() error"); 
 		return -1; 
 	}
 
-	// set local address info
 	sock_addr.sin_family = AF_INET;
 	sock_addr.sin_port = htons(port);
 	sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);		
@@ -28,14 +21,12 @@ int socket_create(int port)
 		return -1; 
 	}
 
-	// bind
 	if (bind(sockfd, (struct sockaddr *) &sock_addr, sizeof(sock_addr)) < 0) {
 		close(sockfd);
 		perror("bind() error"); 
 		return -1; 
 	}
    
-	// begin listening for incoming TCP requests
 	if (listen(sockfd, 5) < 0) {
 		close(sockfd);
 		perror("listen() error");
@@ -44,21 +35,12 @@ int socket_create(int port)
 	return sockfd;
 }
 
-
-
-
-
-/**
- * Create new socket for incoming client connection request
- * Returns -1 on error, or fd of newly created socket
- */
 int socket_accept(int sock_listen)
 {
 	int sockfd;
 	struct sockaddr_in client_addr;
 	socklen_t len = sizeof(client_addr);
 
-	// Wait for incoming request, store client info in client_addr
 	sockfd = accept(sock_listen, (struct sockaddr *) &client_addr, &len);
 	
 	if (sockfd < 0) {
@@ -68,31 +50,21 @@ int socket_accept(int sock_listen)
 	return sockfd;
 }
 
-
-
-
-/**
- * Connect to remote host at given port
- * Returns:	socket fd on success, -1 on error
- */
 int socket_connect(int port, char*host)
 {
 	int sockfd;  					
 	struct sockaddr_in dest_addr;
 
-	// create socket
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
         	perror("error creating socket");
         	return -1;
     }
 
-	// create server address
 	memset(&dest_addr, 0, sizeof(dest_addr));
 	dest_addr.sin_family = AF_INET;
 	dest_addr.sin_port = htons(port);
 	dest_addr.sin_addr.s_addr = inet_addr(host);
 
-	// Connect on socket
 	if(connect(sockfd, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0 ) {
         	perror("error connecting to server");
 		return -1;
@@ -100,13 +72,6 @@ int socket_connect(int port, char*host)
 	return sockfd;
 }
 
-
-
-/**
- * Receive data on sockfd
- * Returns -1 on error, number of bytes received 
- * on success
- */
 int recv_data(int sockfd, char* buf, int bufsize){
 	size_t num_bytes;
 	memset(buf, 0, bufsize);
@@ -117,13 +82,6 @@ int recv_data(int sockfd, char* buf, int bufsize){
 	return num_bytes;
 }
 
-
-
-
-/**
- * Trim whiteshpace and line ending
- * characters from a string
- */
 void trimstr(char *str, int n)
 {
 	int i;
@@ -133,11 +91,6 @@ void trimstr(char *str, int n)
 	}
 }
 
-
-/**
- * Send resposne code on sockfd
- * Returns -1 on error, 0 on success
- */
 int send_response(int sockfd, int rc)
 {
 	int conv = htonl(rc);
@@ -148,12 +101,6 @@ int send_response(int sockfd, int rc)
 	return 0;
 }
 
-
-
-
-/** 
- * Read input from command line
- */
 void read_input(char* buffer, int size)
 {
 	char *nl = NULL;
@@ -161,7 +108,7 @@ void read_input(char* buffer, int size)
 
 	if ( fgets(buffer, size, stdin) != NULL ) {
 		nl = strchr(buffer, '\n');
-		if (nl) *nl = '\0'; // truncate, ovewriting newline
+		if (nl) *nl = '\0';
 	}
 }
 
